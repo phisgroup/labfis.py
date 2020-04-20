7c03d3f3228139fd4e10aeb7d498e7e7808776e4
hlouzada <hendrik.louzada2012@gmail.com>
2020-04-20 16:07:12 -0300

    Fixes to pypi

--- setup.py
+++ setup.py
@@ -31,26 +31,34 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
+with open("README.md", "r") as pd:
+    long_description = pd.read()
+
 version = find_version("labfis", "__init__.py")
-long_description = "Adds a new float class type with an float and uncertainty value"
 
 APP_NAME        = "labfis"
 APP_DESCRIPTION = "Adds a new float type with uncertainty"
 AUTHOR          = "Hendrik Dumith Louzada, João Carlos Rodrigues Júnior"
 AUTHOR_EMAIL    = "hendriklouzada@gmail.com, jc.rodrigues1997@usp.br"
-URL             = "https://github.com/Ovenbird-j/labfis.py"
+URL             = "https://github.com/phisgroup/labfis.py"
+DOWNLOAD_URL    = "https://pypi.org/project/labfis/"
 THEME_NAME      = "Fusion"
 COPYRIGHT       = "Copyright (C) 2020, labfis"
+LICENSE         = "MIT"
 
 
 classifiers = [
     'Development Status :: 4 - Beta',
-    'Framework :: labfis',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
     'Intended Audience :: End Users/Desktop',
     'Intended Audience :: Developers',
     'Operating System :: Microsoft :: Windows',
     'Operating System :: POSIX',
-    'Programming Language :: Python 2.7 :: Python 3 :: Python 3.8']
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8']
 
 python_ver = '>=2.7'
 
@@ -60,7 +68,10 @@ setup(name=APP_NAME,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     url=URL,
+    download_url=DOWNLOAD_URL,
+    license=LICENSE,
     long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     classifiers=classifiers,
     python_requires=python_ver,
