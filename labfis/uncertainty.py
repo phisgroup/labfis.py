7f318fc9edb410d1687ccc8290463b57276dc619
hlouzada <hendrik.louzada2012@gmail.com>
2020-05-08 13:09:17 -0300

    pep8 format

--- labfis/uncertainty.py
+++ labfis/uncertainty.py
@@ -8,18 +8,21 @@ try:
     import numpy
 except ImportError:
     numpy = None
-    logger.warning('Faild to import numpy; sqrt, sin, cos, tan and numpy.ndarray list method wont function')
+    logger.warning(
+        'Faild to import numpy; sqrt, sin, cos, tan and numpy.ndarray list method wont function')
 
-def skipif(condition,message):
+
+def skipif(condition, message):
     def decorator(fnc):
-        def wrapper(*args,**kwargs):
+        def wrapper(*args, **kwargs):
             if condition:
-                return fnc(*args,**kwargs)
+                return fnc(*args, **kwargs)
             else:
                 logger.info(message)
                 pass
         return wrapper
     return decorator
+
 
 class LabFloatError(Exception):
     def __init__(self, *args):
@@ -27,15 +30,19 @@ class LabFloatError(Exception):
             if args[0] == 0:
                 self.message = "This operation is not supported."
             elif args[0] == 1:
-                self.message = "Too many arguments, expected '(val,err)' or '([(val,err),...])' , got: '{0}'".format(args[1])
+                self.message = "Too many arguments, expected '(val,err)' or '([(val,err),...])' , got: '{0}'".format(
+                    args[1])
             elif args[0] == 2:
-                self.message = "Mean list and Uncertainty list must have the same size, expected: '[[val1,val2,...,valn],[err1,err2,...,errn]]' , got: '{0}'".format(args[1:])
+                self.message = "Mean list and Uncertainty list must have the same size, expected: '[[val1,val2,...,valn],[err1,err2,...,errn]]' , got: '{0}'".format(
+                    args[1:])
             elif args[0] == 3:
-                self.message = "Uncertanty or mean list missing, expected: '[[val1,val2,...,valn],[err1,err2,...,errn],...]' , got: '{0}'".format(args[1])
+                self.message = "Uncertanty or mean list missing, expected: '[[val1,val2,...,valn],[err1,err2,...,errn],...]' , got: '{0}'".format(
+                    args[1])
             elif args[0] == 4:
-                self.message = "Too many arguments, expected: '(precision)' or '(mean precision,err precision)' , got: '{0}'".format(args[1])
-            elif isinstance(args[0],str):
-                self.message = "{0}{1}".format(args[0],args[1:])
+                self.message = "Too many arguments, expected: '(precision)' or '(mean precision,err precision)' , got: '{0}'".format(
+                    args[1])
+            elif isinstance(args[0], str):
+                self.message = "{0}{1}".format(args[0], args[1:])
             else:
                 self.message = None
         else:
@@ -47,27 +54,28 @@ class LabFloatError(Exception):
         else:
             return 'A generic LabFloatError has been raised'
 
+
 class labfloat:
-    def __new__(cls, *args,**kwargs):
-        listlabfloat = kwargs.get('list',[])
+    def __new__(cls, *args, **kwargs):
+        listlabfloat = kwargs.get('list', [])
         if args:
             if numpy:
-                if isinstance(args[0],(list,numpy.ndarray)):
+                if isinstance(args[0], (list, numpy.ndarray)):
                     listlabfloat = args
             else:
-                if isinstance(args[0],list):
+                if isinstance(args[0], list):
                     listlabfloat = args
         if listlabfloat != []:
             if len(listlabfloat) % 2 == 0:
                 return cls.list(listlabfloat)
             else:
-                raise LabFloatError(3,listlabfloat)
+                raise LabFloatError(3, listlabfloat)
 
         return object.__new__(cls)
 
-    def __init__(self, *args,**kwargs):
-        mean = kwargs.get('mean',0.0)
-        uncertainty = kwargs.get('uncertainty',0.0)
+    def __init__(self, *args, **kwargs):
+        mean = kwargs.get('mean', 0.0)
+        uncertainty = kwargs.get('uncertainty', 0.0)
 
         if args:
             if len(args) == 1:
@@ -76,22 +84,22 @@ class labfloat:
                 mean = args[0]
                 uncertainty = args[1]
             elif len(args) > 2:
-                raise LabFloatError(1,args)
+                raise LabFloatError(1, args)
 
         self.mean = float(mean)
         self.uncertainty = abs(float(uncertainty))
 
     @classmethod
-    def list(cls,listargs):
+    def list(cls, listargs):
         listlabfloat = []
-        for j in range(0,len(listargs),2):
+        for j in range(0, len(listargs), 2):
             if len(listargs[j]) == len(listargs[j+1]):
                 colum = []
                 for k in range(len(listargs[j])):
-                    colum += [cls(listargs[j][k],listargs[j+1][k])]
+                    colum += [cls(listargs[j][k], listargs[j+1][k])]
                 listlabfloat += [colum]
             else:
-                raise LabFloatError(2,listargs[j],listargs[j+1])
+                raise LabFloatError(2, listargs[j], listargs[j+1])
         if len(listlabfloat) == 1:
             listlabfloat = listlabfloat[0]
         return(listlabfloat)
@@ -103,7 +111,7 @@ class labfloat:
             r = - len(su) + 1
             m = round(self.mean, r)
             u = round(self.uncertainty, r)
-            return((m,u))
+            return((m, u))
         else:
             r = -i
             r += 1
@@ -113,25 +121,25 @@ class labfloat:
                 elif digit != ".":
                     m = round(self.mean, r)
                     u = round(self.uncertainty, r)
-                    return((m,u))
+                    return((m, u))
 
         m = round(self.mean, r)
         u = round(self.uncertainty, r)
-        return((m,u))
+        return((m, u))
 
     def split(self):
         m, u = self.format()
-        return(["{:g}".format(m),"{:g}".format(u)])
-          
-    def tex(self,*args,**kwargs):
+        return(["{:g}".format(m), "{:g}".format(u)])
+
+    def tex(self, *args, **kwargs):
         precision = kwargs.get('precision')
         if args:
             if len(args) == 2:
                 precision = args
             elif len(args) > 2:
-                raise LabFloatError(4,args)
+                raise LabFloatError(4, args)
             else:
-                precision = [args[0],args[0]]
+                precision = [args[0], args[0]]
 
         if self.uncertainty == 0:
             if precision:
@@ -147,7 +155,7 @@ class labfloat:
             return("{0}".format(m))
         else:
             if precision:
-                precision = (str(precision[0]),str(precision[1]))
+                precision = (str(precision[0]), str(precision[1]))
                 m, u = self.format()
                 m = eval("'{:."+precision[0]+"e}'.format(m)")
                 u = eval("'{:."+precision[1]+"e}'.format(u)")
@@ -170,9 +178,9 @@ class labfloat:
 
     def __repr__(self):
         return self.__str__()
-    
+
     def __getitem__(self, idx):
-        vals = [self.mean,self.uncertainty]
+        vals = [self.mean, self.uncertainty]
         return vals[idx]
 
     def __pos__(self):
@@ -208,25 +216,25 @@ class labfloat:
         if isinstance(other, Number):
             return abs(self.mean - other) > 3 * self.uncertainty
 
-    def __lt__(self,other):
+    def __lt__(self, other):
         if isinstance(other, labfloat):
             return self.mean + self.uncertainty < other.mean - other.uncertainty
         if isinstance(other, Number):
             return self.mean + self.uncertainty < other
 
-    def __gt__(self,other):
+    def __gt__(self, other):
         if isinstance(other, labfloat):
             return self.mean - self.uncertainty > other.mean + other.uncertainty
         if isinstance(other, Number):
             return self.mean - self.uncertainty > other
 
-    def __le__(self,other):
+    def __le__(self, other):
         if isinstance(other, labfloat):
             return self.mean + self.uncertainty <= other.mean + other.uncertainty
         if isinstance(other, Number):
             return self.mean + self.uncertainty <= other
 
-    def __ge__(self,other):
+    def __ge__(self, other):
         if isinstance(other, labfloat):
             return self.mean - self.uncertainty >= other.mean - other.uncertainty
         if isinstance(other, Number):
@@ -273,7 +281,7 @@ class labfloat:
 
     def __div__(self, other):
         if isinstance(other, labfloat):
-            return labfloat(self.mean / other.mean, sqrt((self.uncertainty / other.mean) ** 2 + (self.mean * other.uncertainty / (other.mean ** 2)) ** 2 ))
+            return labfloat(self.mean / other.mean, sqrt((self.uncertainty / other.mean) ** 2 + (self.mean * other.uncertainty / (other.mean ** 2)) ** 2))
         if isinstance(other, Number):
             return labfloat(self.mean / other, abs(self.uncertainty / other))
 
@@ -288,7 +296,7 @@ class labfloat:
 
     def __rdiv__(self, other):
         if isinstance(other, labfloat):
-            return labfloat(other.mean / self.mean, sqrt((other.uncertainty / self.mean) ** 2 + (other.mean * self.uncertainty / (self.mean ** 2)) ** 2 ))
+            return labfloat(other.mean / self.mean, sqrt((other.uncertainty / self.mean) ** 2 + (other.mean * self.uncertainty / (self.mean ** 2)) ** 2))
         if isinstance(other, Number):
             return labfloat(other / self.mean, abs(other * self.uncertainty / self.mean ** 2))
 
@@ -310,19 +318,19 @@ class labfloat:
     def __ipow__(self, other):
         return self.__pow__(other)
 
-    @skipif(numpy,"The sqrt() method is not supported without numpy")
+    @skipif(numpy, "The sqrt() method is not supported without numpy")
     def sqrt(self):
         return self.__pow__(0.5)
 
-    @skipif(numpy,"The cos() method is not supported without numpy")
+    @skipif(numpy, "The cos() method is not supported without numpy")
     def cos(self):
-        return labfloat(cos(self.mean),abs(-(sin(self.mean)) * self.uncertainty))
-    
-    @skipif(numpy,"The sin() method is not supported without numpy")
-    def sin(self):
-        return labfloat(sin(self.mean),abs(cos(self.mean) * self.uncertainty))
+        return labfloat(cos(self.mean), abs(-(sin(self.mean)) * self.uncertainty))
 
-    @skipif(numpy,"The tan() method is not supported without numpy")
+    @skipif(numpy, "The sin() method is not supported without numpy")
+    def sin(self):
+        return labfloat(sin(self.mean), abs(cos(self.mean) * self.uncertainty))
+
+    @skipif(numpy, "The tan() method is not supported without numpy")
     def tan(self):
         return labfloat(tan(self.mean), sqrt((cos(self.mean) ** -4) * self.uncertainty ** 2))
 
