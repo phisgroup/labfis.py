57b65734ef86c718faa00d59fe988101b0d2d24e
hlouzada <hendrik.louzada2012@gmail.com>
2020-05-18 20:44:02 -0300

    Enh test efficency and precision

--- tests/test_labfloat.py
+++ tests/test_labfloat.py
@@ -1,9 +1,10 @@
 import pytest
 import numpy
+import itertools
 from math import sqrt
 import labfis
 
-dx = 1e-13
+dx = 1e-10
 
 
 def labfloat_iterative(vardic, exp):
@@ -38,29 +39,34 @@ def labfloat_calc(vardic, exp):
 
 rng = numpy.random.default_rng()
 
-expresions = [
-    "a*b*c",
-    "a*b/c",
-    "(a**b)*c",
-    "2/b*(a/c)",
-    "a+b+c",
-    "a-b+c",
-    "c**b/a",
-    "(c**0.5+b**1.3)*"+str(numpy.e)+"**a"
-]
+operations = ["*", "/", "+", "-", "**"]
 
 vals = {
     "a": (rng.random(), rng.random()),
     "b": (rng.random(), rng.random()),
-    "c": (rng.random(), rng.random())
+    "c": (rng.random(), rng.random()),
+    "d": (rng.random(), rng.random()),
+    "e": (rng.random(), rng.random())
 }
 
 
 def test_labfloat_expressions():
-    for exp in expresions:
-        result1 = labfloat_iterative(vals, exp)
-        result2 = labfloat_calc(vals, exp)
+    print(vals)
+    var = list(vals.keys())
+    opers = itertools.combinations_with_replacement(operations, len(var)-1)
+    for exp in opers:
+        expression = ""
+        for i in range(len(exp)):
+            if i == len(exp)-1:
+                expression += var[i]+exp[i]+var[i+1]
+            else:
+                expression += var[i]+exp[i]
 
+        result1 = labfloat_iterative(vals, expression)
+        result2 = labfloat_calc(vals, expression)
+
+        print(expression)
         print(result1, result2)
+        print(round(result1), round(result2))
 
         assert round(result2) == round(result1)
