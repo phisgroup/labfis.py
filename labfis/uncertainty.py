9184ae8c900f433031d0bb83b11f3f96ea4fcffd
hlouzada <hendrik.louzada2012@gmail.com>
2020-06-08 21:17:16 -0300

    pep8 formating

--- labfis/uncertainty.py
+++ labfis/uncertainty.py
@@ -24,6 +24,7 @@ def skipif(condition, message):
         return wrapper
     return decorator
 
+
 class Infix:
     """definition of an Infix type operator class
     also works in jython,
@@ -31,16 +32,19 @@ class Infix:
     x |infix| y
     or:
     x <<infix>> y"""
+
     def __init__(self, function):
         self.function = function
 
     def __ror__(self, other):
         return self.lbind(self.function, other)
+
     def __or__(self, other):
         return self.rbind(self.function, other)
 
     def __rlshift__(self, other):
         return self.lbind(self.function, other)
+
     def __rshift__(self, other):
         return self.rbind(self.function, other)
 
@@ -48,10 +52,13 @@ class Infix:
         def __init__(self, function, binded):
             self.function = function
             self.binded = binded
+
         def __ror__(self, other):
             return self.function(other, self.binded)
+
         def __rlshift__(self, other):
             return self.function(other, self.binded)
+
         def __call__(self, other):
             return self.function(other, self.binded)
 
@@ -59,12 +66,16 @@ class Infix:
         def __init__(self, function, binded):
             self.function = function
             self.binded = binded
+
         def __or__(self, other):
             return self.function(self.binded, other)
+
         def __rshift__(self, other):
             return self.function(self.binded, other)
+
         def __call__(self, other):
             return self.function(self.binded, other)
+
 
 class LabFloatError(Exception):
     def __init__(self, *args):
