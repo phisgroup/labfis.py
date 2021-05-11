50380f0276e7067034546abeabd88f3e2851ef3e
jc.rodrigues1997@usp.com <jc.rodrigues1997@usp.com>
2021-05-11 16:57:06 -0300

    ADD: testing the obj initialization

--- labfis/test/unit_test.py
+++ labfis/test/unit_test.py
@@ -6,7 +6,13 @@ def test_infix():
         x, e = uniform(-2e50,2e50), uniform(-2e50,2e50) 
         assert labfloat(x, e) == x |u| e == x <<u>> e
 
+class TestInitialization():
+    def test_empty_eq_zero(self):
+        assert labfloat() == labfloat(0, 0) == labfloat(0.0, 0.0)
 
+    def test_int_eq_float(self):
+        assert labfloat(10, 2) == labfloat(10., 2.) 
 
-
-
+    def test_attrs(self):
+        x = labfloat(10, 1)
+        assert x.mean == 10 and x.uncertainty == 1
