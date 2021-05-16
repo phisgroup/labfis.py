16a504b567c61416097f9e4c51e9a3e07918bc5d
hlouzada <hendrik.louzada2012@gmail.com>
2021-05-16 00:29:22 -0300

    Fix: comparison in tests and code refactor

--- labfis/test/arithmetic_test.py
+++ labfis/test/arithmetic_test.py
@@ -1,23 +1,37 @@
-from labfis import labfloat as lf
 from itertools import combinations
+from labfis import labfloat
+
 
 def test_zeros():
-    zeros = [lf(),lf(0),lf(0.),lf(0,0),
-            lf(0.,0.),lf(0.,0),lf(0,0.),0,0.]
+    zeros = [labfloat(), labfloat(0), labfloat(0.), labfloat(0, 0),
+             labfloat(0., 0.), labfloat(0., 0), labfloat(0, 0.)]
+
     for z1, z2 in combinations(zeros, 2):
-        assert z1 == z2 
+        print(z1, z2)
+        assert list(z1) == list(z2)
 
 def test_indexing():
-    assert lf(10,2)[:] == [10,2]
+    assert labfloat(10, 2)[:] == (10, 2)
+
 
 def TestUnary():
-    def test_pos(self):
-        x = lf(100,4)
-        assert x == +x
+    def test_pos():
+        x = labfloat(100, 4)
+        y = +x
 
-    def test_neg():
-        x = lf(243,2)
-        y = lf(-243,2)
-        assert x == -y
+        assert list(x) == list(y)
 
-#TODO: test cases for other arithmetic operators
+    def test_neg_mean():
+        x = labfloat(243, 2)
+        y = labfloat(-243, 2)
+        y = -y
+
+        assert list(x) == list(y)
+
+    def test_neg_uncertainty():
+        x = labfloat(243, 2)
+        y = labfloat(243, -2)
+
+        assert list(x) == list(y)
+
+# TODO: test cases for other arithmetic operators
